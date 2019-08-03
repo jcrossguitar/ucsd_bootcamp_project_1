9e2a2b5756fc316a2ec69dd5eb8d9e201e5421cf
kenny the tomato <jcrossguitar@gmail.com>
2019-08-03 12:54:41 -0700

    I am working on the AJAX and receiving an error message

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,21 +1,29 @@
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js"></script>
+// // <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js"></script>
 
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#config-web-app -->
+// // <!-- TODO: Add SDKs for Firebase products that you want to use
+// //      https://firebase.google.com/docs/web/setup#config-web-app -->
 
-    var firebaseConfig = {
-    apiKey: "AIzaSyACyjZR80fGfMU6E8m9wp-2GZoTm37xkc4",
-    authDomain: "activity-9bee5.firebaseapp.com",
-    databaseURL: "https://activity-9bee5.firebaseio.com",
-    projectId: "activity-9bee5",
-    storageBucket: "",
-    messagingSenderId: "107365426255",
-    appId: "1:107365426255:web:ea28b339d9c4eb40"
-}
-  firebase.initializeApp(firebaseConfig);
+//     var firebaseConfig = {
+//     apiKey: "AIzaSyACyjZR80fGfMU6E8m9wp-2GZoTm37xkc4",
+//     authDomain: "activity-9bee5.firebaseapp.com",
+//     databaseURL: "https://activity-9bee5.firebaseio.com",
+//     projectId: "activity-9bee5",
+//     storageBucket: "",
+//     messagingSenderId: "107365426255",
+//     appId: "1:107365426255:web:ea28b339d9c4eb40"
+// }
+//   firebase.initializeApp(firebaseConfig);
+
+var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyBmm3VMGrk19WBVK6bPPAr639waetupjs0"
 
 
+$(document).ready(function() {
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+    })
 
 
 
