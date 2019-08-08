13a707364df0a49bd8cdf2f0ec4d874bfc2d9b11
Juddhalenza <51768301+Juddhalenza@users.noreply.github.com>
2019-08-07 20:55:40 -0700

    pushing carousel and some changes to strength JS and style sheet

---
+++ senior/senior.js
@@ -0,0 +1,33 @@
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+  <h1 class="display-4">Yoga Stuff and Things</h1>
+  <p class="lead">This is your introduction to finding yoga that's right for you.</p>
+</div>
+</div>  
+
+<div class="card-deck">
+<div class="card">
+  <img src="assets/images/balance rocks.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Strength & Flexibility</h5>
+    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card">
+  <img src="assets/images/lotus flower.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Stress Relief & Meditation</h5>
+    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card">
+  <img src="assets/images/gratitude-sunrise.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Slow Yoga & Senior Yoga</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
