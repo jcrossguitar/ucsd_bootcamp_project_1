973a0b2d9faea7f99e8f6f2072ddc7b494e31c70
jon <jcrossguitar@gmail.com>
2019-08-12 09:41:32 -0700

    I created a weather page and added the info the the landing page. I will need to change the other nav bars with the info, but it is a quick fix.

---
+++ Weather/weather.js
@@ -0,0 +1,28 @@
+
+
+        // creates a click function for submitting the city searched
+            $(document).ready(function() {
+                $("#submitWeather").click(function(){
+                    
+                    // creating a variable for city
+                    var city = $("city").val();
+
+        // if the city value is not blank, run ajax call to request information from the server
+                    if(city !=''){
+        // ajax call
+                        $.ajax({
+                            url: "https://api.openweathermap.org/data/2.5/forecast/weather?q=" + city + 
+                            "APPID=d4fd3719101afa0124c6ea3df4c057b5",
+                            type: 'GET',
+                            dataType: "jsonp",
+                            success: function(data){
+                                console.log(data)
+                            }
+                        })
+        // display an error if form submitted blank
+                    }else{
+                        $("#error").html('Field cannot be empty');
+                    }
+        
+                });
+            });
